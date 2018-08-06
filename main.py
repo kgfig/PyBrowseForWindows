3ca82f1529d31324194d13abb55cdb73ddc2a94e
unknown <t4@gmail.com>
2018-08-06 17:13:05 +0800

    Open a browser using PyWebview

---
+++ main.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -8-
+import webview
+# Create a resizable window
+webview.create_window("PyBrowse", "https://moosystems.com", width=800, height=600, resizable=True, fullscreen=False)
