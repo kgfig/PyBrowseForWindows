4fcdb27e136fcc17ea31440a7daabe832c9c3682
unknown <t4@gmail.com>
2018-08-11 12:28:24 +0800

    Update logging config

--- main.py
+++ main.py
@@ -14,10 +14,19 @@ except NameError:
     base_dir = os.path.realpath(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0]))))
     run_as_binary = True
 
-log_msg = 'Are we running as binary? %s' % run_as_binary
 
 # Setup logging
-log_file = os.path.join(base_dir, 'PyBrowse for Windows.log')
+if run_as_binary:
+    log_file = os.path.join(base_dir, '..', 'PyBrowse for Windows.log')
+    cherry_access_log = os.path.join(base_dir, '..', 'access.log')
+    cherry_error_log = os.path.join(base_dir, '..', 'error.log')
+    app_name = 'PyBrowse for Windows'
+else:
+    log_file = os.path.join(base_dir, '..', 'PyBrowse for Windows.log')
+    cherry_access_log = os.path.join(base_dir, 'access.log')
+    cherry_error_log = os.path.join(base_dir, 'error.log')
+    app_name = 'PyBrowse for Windows'
+
 handler = logging.handlers.RotatingFileHandler(log_file, backupCount=10)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(message)s')
@@ -27,8 +36,9 @@ log = logging.getLogger('PyBrowse for Windows')
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
 
-# py2exe will report an error even if there's none because we logged something here
-log.debug(log_msg) 
+# py2exe will report an error even if there's none if we log something
+# log_msg = 'Are we running as binary? %s' % run_as_binary
+# log.debug(log_msg)
 
 # Create a resizable window
 webview.create_window("PyBrowse for Windows", "https://moosystems.com", width=800, height=600, resizable=True, fullscreen=False)
